d0bdb2bacb2acb04f0ec14955449d024a314479c
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-07-16 20:33:36 -0700

    Rainbow colors, continuously generated

--- src/main.rs
+++ src/main.rs
@@ -19,28 +19,6 @@ impl Color {
     }
 }
 
-struct Palette {
-    options:  Vec<Color>,
-    backdrop: Color
-}
-
-impl Palette {
-    fn new(size: usize) -> Self {
-        let options = (0..size).into_iter().map(|i| {
-            Color {
-                r: rand::random::<u8>(),
-                g: rand::random::<u8>(),
-                b: rand::random::<u8>()
-            }
-        }).collect::<Vec<Color>>();
-
-        Palette {
-            options:  options,
-            backdrop: Color { r: 0, g: 0, b: 0 }
-        }
-    }
-}
-
 fn complex_from_point(x: u32, y: u32, xmax: u32, ymax: u32) -> Complex<f32> {
     Complex {
         re: (x as f32 / xmax as f32) * 3.0 - 2.5,
@@ -48,12 +26,17 @@ fn complex_from_point(x: u32, y: u32, xmax: u32, ymax: u32) -> Complex<f32> {
     }
 }
 
-fn mandelbrot_escape_number(complex_point: Complex<f32>) -> Option<usize> {
+fn mandelbrot_escape_number(complex_point: Complex<f32>, escape_point: Option<u32>) -> Option<usize> {
     let mut z: Complex<f32> = Complex { re: 0.0, im: 0.0 };
+
+    let epoint = match escape_point {
+        Some(value) => value as f32,
+        None        => 2.0
+    };
 
     for i in 0..250 {
         z = z.powf(2.0) + complex_point;
-        if z.norm() >= 2.0 {
+        if z.norm() >= epoint {
             return Some(i)
         }
     }
@@ -61,23 +44,35 @@ fn mandelbrot_escape_number(complex_point: Complex<f32>) -> Option<usize> {
     None
 }
 
-fn choose_color<'a>(escape_number: Option<usize>, palette: &'a Palette) -> &'a Color {
+fn choose_color<'a>(escape_number: Option<usize>) -> Color {
     match escape_number {
-        Some(i) => &palette.options[i as usize % palette.options.len()],
-        None    => &palette.backdrop
+        Some(i) => {
+            let frequency = 0.3;
+            let phase_r   = 0.0;
+            let phase_g   = 2.0;
+            let phase_b   = 4.0;
+            let width     = 127.0;
+            let center    = 128.0;
+
+            Color {
+                r: ((frequency * i as f32 + phase_r).sin() * width + center) as u8,
+                g: ((frequency * i as f32 + phase_g).sin() * width + center) as u8,
+                b: ((frequency * i as f32 + phase_b).sin() * width + center) as u8
+            }
+        },
+
+        None => Color { r: 0, g: 0, b: 0 }
     }
 }
 
 fn main () {
-    let xdim: u32 = 5600;
-    let ydim: u32 = 3200;
-
-    let palette = Palette::new(15);
+    let xdim: u32 = 1400;
+    let ydim: u32 = 800;
 
     let imgbuf = image::ImageBuffer::from_fn(xdim, ydim, |x, y| {
         let complex       = complex_from_point(x, y, xdim, ydim);
-        let escape_number = mandelbrot_escape_number(complex);
-        let color         = choose_color(escape_number, &palette);
+        let escape_number = mandelbrot_escape_number(complex, None);
+        let color         = choose_color(escape_number);
 
         color.to_pixel()
     });
